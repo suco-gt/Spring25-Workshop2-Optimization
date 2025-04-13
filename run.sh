e913b2f56004868b858de21ad40d5117a76b6585
Min Lu <mlu316@gatech.edu>
2025-04-12 22:03:38 -0400

    added bash command to run intro py file

--- run.sh
+++ run.sh
@@ -4,6 +4,10 @@ if [ "$1" == "test" ]; then
     mpiexec -n "$SLURM_NTASKS_PER_NODE" python hello_world.py
 fi
 
+if [ "$1" == "intro" ]; then
+    mpiexec -n "$SLURM_NTASKS_PER_NODE" python solutions/0-mpi-intro.py
+fi
+
 if [ "$1" == "brute" ]; then
     mpiexec -n "$SLURM_NTASKS_PER_NODE" python main.py -f tests/input.txt -o brute
 fi
